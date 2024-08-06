b477465648987d87fe7989314232d4bdbbe14717
dyald2h93 <adolfo.jgi.hernandez@gmail.com>
2024-08-06 21:56:00 +0000

    add changes

--- portal_beneficiario/public/js/basic_data/index.js
+++ portal_beneficiario/public/js/basic_data/index.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
-
-    console.log("aqui");    
+   
 
     if($('#link_date')){
         $('#link_date').datepicker();
@@ -21,4 +20,46 @@ $( document ).ready(function() {
             $('.fpep_fields').attr('hidden', true);
         }
     });
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+        var target = $(e.target);
+    
+        if (target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+
+        var active = $('.wizard .nav-tabs li.active');
+        active.next().removeClass('disabled');
+        nextTab(active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var active = $('.wizard .nav-tabs li.active');
+        prevTab(active);
+
+    });
+});
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
+
+
+$('.nav-tabs').on('click', 'li', function() {
+    var li_active = $('.nav-tabs li.active');
+    var div_active = li_active.find('a[data-toggle="tab"]').attr('aria-controls');
+    $(`#${div_active}`).removeClass('active');
+    li_active.removeClass('active');
+    $(this).addClass('active');
+    var this_div = $(this).find('a[data-toggle="tab"]').attr('aria-controls')
+    $(`#${this_div}`).addClass('active');
 });
